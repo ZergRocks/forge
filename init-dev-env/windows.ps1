16d29fd66c7dd8695749effe6a5f2ced91c9f039
ZergeRocks Windows <forhelsinki21@gmail.com>
2024-07-10 01:17:29 +0900

    [style] Change explicit powershell function name call

--- init-dev-env/windows.ps1
+++ init-dev-env/windows.ps1
@@ -5,7 +5,7 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; `
   [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol `
   -bor 3072; `
-  iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+  Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # 3. install choco packages
 # choco install microsoft-windows-terminal -y # no need to install
